75d52231262f573fb483d7529232586d0bb77318
Lander <d_dmitry@standart-n.ru>
2020-09-18 10:53:19 +0400

    Export warebase for 103KZ,2GIS and PharmIT

--- PYTHON/export_csv_tomail/declare.py
+++
@@ -1,6 +0,0 @@
-# Строка подключение к БД
-
-host = '192.168.67.33'
-# db_file = 'E:\Clients/aibolit/aibolit28\ztrade.fdb'
-db_file = 'd:\Standart-N/base_s_new/ZTRADE_S.FDB'
-
--- PYTHON/export_csv_tomail/export.py
+++ PYTHON/export_csv_tomail/export.py
@@ -1,12 +1,13 @@
-import declare
+﻿#  Autor by Lander (c) 2020. Created for Standart-N LLT
+
 import fdb
 import csv
 import configparser
-import zipfile
 from ftplib import FTP
+import zipfile 
 
 config = configparser.ConfigParser()
-config.read('./config.ini')
+config.read('c:/STANDART-N/export_csv_tomail/config.ini')
 section = 'BASE_CONF'
 host = config.get(section, 'HOST')
 database = config.get(section, 'PATH')
@@ -17,12 +18,9 @@ conn = fdb.connect(host=host, database=database, user='sysdba', password='master
 
 curs = conn.cursor()
 head = [config.get('CSV_CONF', 'HEAD')]
-SQL_profile= "SELECT id,caption,email FROM G$PROFILES"
-SQL_warebase = "SELECT sname, cast(quant as numeric(9,2)), price FROM warebase_g  w "
-
-SQL_warebase_PharmIT = "select (select caption from g$profiles g where g.id=w.g$profile_id),sname,sizg,quant,price from warebase_g w  where w.quant>0"
-#SQL_warebase_PharmIT = "select g.caption,w.sname,w.sizg,w.quant,w.price from warebase_g w inner join g$profiles g on g.id=w.g$profile_id and g.email is not null  where w.quant>0"
-
+SQL_profile= "SELECT id,caption,email FROM G$PROFILES where email is not null"
+SQL_warebase = "SELECT sname, cast(quant as numeric(9,2)), price FROM warebase_g w "
+SQL_warebase_PharmIT = "select g.caption,w.sname,w.sizg,w.quant,w.price from warebase_g w inner join g$profiles g on g.id=w.g$profile_id and g.email is not null  where w.quant>0"
 #запрос к базе
 def get_sql(sql,where=None):
     if where is None:
@@ -37,11 +35,12 @@ def get_sql(sql,where=None):
         result.append(list(i))
     return result
 
-def upload_FTP(host,ftp_user,ftp_password,file_name,port):
+def upload_FTP(host,ftp_user,ftp_password,file_name,port=21):
     ftp = FTP()
-
     ftp.connect(host,port)
+
     ftp.login(ftp_user, ftp_password)
+    
     ftp.set_pasv(True)
     ftp.storlines("STOR " + file_name, open(file_name, 'rb'))
     ftp.quit()
@@ -61,21 +60,23 @@ def create_csv(filename,data,header=None,profile_code=None):
             else:
                 row=[profile_code]+row
             writer.writerow(row)
-            #print(row)
 
+profiles = get_sql(SQL_profile)
+for i in profiles:
+    where = "where w.quant>0 and w.g$profile_id="+str(i[0])
+    #Выгрузка остатков раздельно по профилям
+    create_csv(i[2],get_sql(SQL_warebase,where),head,i[2])
+    # Выгружаем на FTP  apteka.103.kz
+    upload_FTP(config.get('FTP_CONF', 'FTP_HOST'),config.get('FTP_CONF', 'FTP_USER'),config.get('FTP_CONF', 'FTP_PASSWORD'),i[2]+'.csv')
+    #Выгружаем на FTP  2Gis
+    upload_FTP(config.get('FTP_CONF1', 'FTP_HOST'), config.get('FTP_CONF1', 'FTP_USER'),
+               config.get('FTP_CONF1', 'FTP_PASSWORD'), i[2]+'.csv')
 
-#profiles = get_sql(SQL_profile)
-
-# for i in profiles:
-#     where = "where w.g$profile_id="+str(i[0])
-#     create_csv(i[1],get_sql(SQL_warebase,where),head,i[2])
-    # upload_FTP(config.get('FTP_CONF', 'HOST'),config.get('FTP_CONF', 'FTP_USER'),config.get('FTP_CONF', 'FTP_PASSWORD'),i[1].'.csv')
-    # upload_FTP(config.get('FTP_CONF1', 'HOST'), config.get('FTP_CONF1', 'FTP_USER'),
-    #            config.get('FTP_CONF1', 'FTP_PASSWORD')], i[1].
-    # '.csv')
-
-create_csv('aya', get_sql(SQL_warebase_PharmIT))#формирует выгрузку остатков для фармИТ
-upload_FTP(config.get('FTP_CONF2', 'FTP_HOST'),config.get('FTP_CONF2', 'FTP_USER'),config.get('FTP_CONF2', 'FTP_PASSWORD'),'3.csv',int(config.get('FTP_CONF2', 'FTP_PORT')))
-
-
+#формирует выгрузку остатков для фармИТ
+filen=config.get('FTP_CONF2', 'ID_CLIENT')
+create_csv(filen, get_sql(SQL_warebase_PharmIT))
+PharmZIP=zipfile.ZipFile('./'+filen+'.zip','w')
+PharmZIP.write('./'+filen+'.csv',compress_type=zipfile.ZIP_DEFLATED)
+PharmZIP.close()
+upload_FTP(config.get('FTP_CONF2', 'FTP_HOST'),config.get('FTP_CONF2', 'FTP_USER'),config.get('FTP_CONF2', 'FTP_PASSWORD'),filen+'.csv',int(config.get('FTP_CONF2', 'FTP_PORT')))
 
