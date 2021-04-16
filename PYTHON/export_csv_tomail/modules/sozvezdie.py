3b6555d933b0bc2458287da011e6a644c3949bc8
Lander <d_dmitry@standart-n.ru>
2021-04-16 13:27:07 +0400

    добавлена третья часть выгрузка остатков по дням движения товара Для Созвездия.

--- PYTHON/export_csv_tomail/modules/sozvezdie.py
+++ PYTHON/export_csv_tomail/modules/sozvezdie.py
@@ -2,6 +2,7 @@
 from engine import FTP_work,Archiv,Db,os,LogIt,existPath,read_ini,get_File,put_file
 from XMLcreate import XML
 import datetime
+import itertools
 
 class Sozvezdie(Db):
     def __init__(self,profile_id=None):
@@ -28,56 +29,72 @@ class Sozvezdie(Db):
 
 
 
-    def create_export(self,gl_root,root,element,data,head_file,sql,date_start=None,date_end=None):
+    def create_export(self,root,element,data,head_file,sql,date_start=None,date_end=None,gl_root=None):
         self.doc_dates=[]
         if self.type:
             attrib = {"type": root,  "map_pharmacy_ids": self.dep_code}
         else:
             attrib={"type":root,"datestart":self.date_start.strftime("%Y-%m-%dT%H:%M:%S"),"dateend":self.date_end.strftime("%Y-%m-%dT23:59:59"),"map_pharmacy_ids":self.dep_code}
 
-        rt=XML().add_element(root=gl_root,element='action',attrib=attrib)
-        el=XML().add_element(root,rt)
+        if gl_root is not None:
+            self.rt=XML().add_element(root=gl_root,element='action',attrib=attrib)
+            self.el=XML().add_element(root,self.rt)
         sql = get_File(path='./modules/sozvezdie/sql/', file=sql)
+        #data = data if type(data) ==int else list(set(data))
+        x = 0
         for pid in data:
+
+            da_strt = self.date_start if type(pid)==int else pid[1]
+            da_end = self.date_end if type(pid)==int  else pid[1]
+            da_end = self.date_start if date_end else da_end
+            pid = str(pid) if type(pid) == int else pid[0]
             sql1 = sql.replace(':pid', str(pid))
-            sql1 = sql1.replace(':date_start',"'"+self.date_start.strftime('%d.%m.%Y:00:00:00')+"'")
-            sql1 = sql1.replace(':date_end', "'"+self.date_end.strftime('%d.%m.%Y:23:59:59')+"'")
+            sql1 = sql1.replace(':date_start',"'"+da_strt.strftime('%d.%m.%Y:00:00:00')+"'")
+            sql1 = sql1.replace(':date_end', "'"+da_end.strftime('%d.%m.%Y:23:59:59')+"'")
             p = self.DB.get_sql(sql1)
 
             # el1=XML().add_element(element,el)
             heads = get_File('./modules/sozvezdie/head/', head_file)
             heads = heads.split('\n')
 
-            if 'move' in head_file:
-                self.doc_dates.append([p[0][18],p[0][2]])
+            if date_start and da_strt.strftime('%d.%m.%Y:00:00:00') == date_start.strftime('%d.%m.%Y:00:00:00'):
+                print(head_file, da_strt, da_end, date_start)
+                continue
+
             for row in p:
-                el1 = XML().add_element(element, el)
+
+                el1 = XML().add_element(element, self.el)
+                if 'move' in head_file :
+                    self.doc_dates.append([row[2], row[18]])
 
                 y = 0
                 for head in heads:
-                    exp_data= row[y]
-
+                    exp_data = row[y]
+                    # if 'base' in head_file and x==0 and y in (3,5,7):
+                    #     exp_data='0.0'
                     if head != 'nomenclature_codes':
                         XML().add_subelement(subelem=head,elem=el1,data=exp_data)
                     else:
                         el2 = XML().add_element(head,el1)
                         XML().add_subelement(subelem='code', elem=el2, data=exp_data,attrib={"owner":"map"})
                     y += 1
-        return rt
+            x += 1
+        return self.rt
 
     def get_Data(self):
         self.get_Date()
         file_name = self.get_Filename()
         part_id=[]
         part_id1=[]
-        sql = 'SELECT first 200 part_id from docs d left join doc_detail dd on dd.doc_id=d.id'
+        sql = 'SELECT part_id from docs d left join doc_detail dd on dd.doc_id=d.id'
         if self.type:
             where = f" where d.docdate < '{self.date_start.strftime('%d.%m.%Y:00:00:00')}'and part_id is not null group by dd.part_id having (abs(sum(dd.quant))>0.001)"
+            #where = f" where d.docdate < '{self.date_start.strftime('%d.%m.%Y:00:00:00')}'and part_id =126608 group by dd.part_id having (abs(sum(dd.quant))>0.001)"
             p = self.DB.get_sql(sql, where)
             for po in p:
                 part_id1.append(po[0])
             part_id1 = list(set(part_id1))
-
+        #where = f" where d.docdate between '{self.date_start.strftime('%d.%m.%Y:00:00:00')}' and '{self.date_end.strftime('%d.%m.%Y:23:59:59')}' and part_id =126608"
         where = f" where d.docdate between '{self.date_start.strftime('%d.%m.%Y:00:00:00')}' and '{self.date_end.strftime('%d.%m.%Y:23:59:59')}' and part_id is not null"
         p = self.DB.get_sql(sql, where)
         for po in p:
@@ -96,15 +113,23 @@ class Sozvezdie(Db):
             self.create_export(gl_root=gl_root,root='distributions', element='distribution', data=part_id, head_file='move', sql='action_move')
 
         docs = self.doc_dates
-        # for d in docs:
-        #     LogIt(str(d[0])+' '+str(d[1]))
+        docs.sort()
+        docs = list(docs for docs,_ in itertools.groupby(docs) )
+
+        if self.type:
+
+            self.create_export(gl_root=gl_root, root='remnants', element='remnant', data=part_id1, head_file='base', sql='action_remnant_first',date_end='1')
+
+
+            self.create_export(gl_root=None,root='remnants',element='remnant', data=docs, head_file='base', sql='action_remnant',date_start=self.date_start)
+        else:
+            self.create_export(gl_root=gl_root, root='remnants', element='remnant', data=docs, head_file='base', sql='action_remnant')
+
 
 
         XML().save_file(root=gl_root,filename=file_name)
         LogIt('END')
-        exit()
-
-        self.get_Date()
+ 
 
 
 
