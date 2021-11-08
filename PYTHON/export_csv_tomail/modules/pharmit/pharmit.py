e5d76313e2fecd3a688b2e4e2fc52b3a26938cea
lander <lander@udm.ru>
2021-11-08 13:35:21 +0400

    Алгоритм PharmIt Реализован

--- PYTHON/export_csv_tomail/modules/pharmit/pharmit.py
+++ PYTHON/export_csv_tomail/modules/pharmit/pharmit.py
@@ -1,16 +1,13 @@
 #  Autor by Lander (c) 2021. Created for Standart-N LLT
 import json
 import sys
-
 from engine import Db, existPath, read_ini, my_log, put_file
 import requests
 import datetime
 from requests.auth import HTTPBasicAuth
-
+from calendar import monthrange
 
 logger = my_log.get_logger(__name__)
-
-
 class Pharmit(Db):
     def __init__(self, profile_id=None):
         self.DB = Db()
@@ -27,16 +24,22 @@ class Pharmit(Db):
         self.DB.cheak_db(read_ini(self.conf, 'TRIGGER', self.path_ini), 'TRIGGER')
         self.auth=HTTPBasicAuth(read_ini(self.conf, 'LOGIN', self.conf),read_ini(self.conf, 'PASS', self.conf))
 
-    def get_Data(self):
-        self.getDate()
+    def get_Data(self,date_start=None):
+        if not date_start:
+            self.getDate()
+        else:
+            self.date_start=date_start
+
         print(self.profile_id)
         val = {'date_start': self.date_start, 'date_end': self.date_start, 'profile_id': self.profile_id}
         self.data = self.DB.get_from_base(__name__, 'move', val)
-        #date_start = datetime.date.today()-datetime.timedelta(days=11)
+        # date_start = datetime.date.today()-datetime.timedelta(days=11)
         self.fileID = self.getToken(datetime.datetime.strptime(self.date_start,'%d.%m.%Y').strftime('%Y%m%d'))
-        print(f'FileID-{self.fileID}')
-        c=self.getPacket()
-        self.put_packet(c)
+        #print(f'FileID-{self.fileID}')
+        self.put_packet(self.getPacket())
+        if not date_start:
+            self.check_date()
+        self.get_Decada()
 
     def send_request(self, type,data=None,header=None,auth=None,url=None):
         header = {'Content-type': 'application/json'} if type=='POST' else None
@@ -45,12 +48,14 @@ class Pharmit(Db):
         return res.text
 
 
-    def getToken(self, date):
+    def getToken(self, date,type='DateSale'):
         headers = {'Content-type': 'application/json', 'Accept': 'application/json'}
-        data = json.dumps({'DateSale': date})
+        data = json.dumps({type: date})
         res=self.send_request(data=data,type='POST',header=headers,auth=self.auth,url=self.url_file_id)
         if "Данные за эту дату уже загружены!" in res:
-            res=self.send_request(url=self.url_get_file_id+date,type='GET',auth=self.auth)
+            logger.error("Данные за эту дату уже загружены!")
+            sys.exit()
+            #res=self.send_request(url=self.url_get_file_id+date,type='GET',auth=self.auth)
         return res
 
     def getPacket(self):
@@ -81,6 +86,7 @@ class Pharmit(Db):
                        'ProviderSaleId': pack[15],
                        'FileId': self.fileID,
                        'QuantityBalance': str(pack[17]), }
+            self.check_null(pack)
             packet.append(one_str)
             i += 1
             if i == 100:
@@ -112,7 +118,76 @@ class Pharmit(Db):
             self.date_start=read_ini(self.conf,'DATE_START',self.conf)
         return self.date_start
 
+    def check_null(self,packet):
+       # TypeId, GoodName, Pharm, Quantity, FileId, ProviderSaleId
+        if not packet[0]  or not packet[4]  or not packet[8]  or not packet[5]  or not packet[15] :
+            logger.error(f'Пустые значения -TypeId {packet[0]} GoodName {packet[4]} Pharm {packet[8]} Quantity {packet[5]} ProviderSaleId {packet[15]}')
 
+    def check_date(self):
+        #return
+        chk_date=datetime.datetime.strptime(read_ini(self.conf,'DATE_CHECK',self.conf), '%d.%m.%Y').strftime('%Y%m%d')
+        url=f'{self.url_load}?startDate={chk_date}'
+        res = self.send_request(url=url, type='GET', auth=self.auth)
+        res = json.loads(res.replace("'",'"'))
+        noDate=res['noDates']
+        for date in noDate:
+            c_date=datetime.datetime.strptime(str(date),'%Y%m%d').strftime('%d.%m.%Y')
+            self.get_Data(date_start=c_date)
+
+    def getPacket_decada(self):
+        packet = []
+        i = 1
+        j = 0
+        for pack in self.data:
+
+            one_str = {'typeid': pack[0],
+                       'Tender': pack[1],
+                       'GoodName': pack[2],
+                       'producerName': pack[3],
+                       'Pharm': pack[4],
+                       'Pharmaddress': pack[5],
+                       'barcode': str(pack[6]),
+                       'quantity': pack[7],
+                       'FileId': self.fileID,
+         }
+            self.check_null(pack)
+            packet.append(one_str)
+            i += 1
+            if i == 100:
+                i = 0
+                self.send_packet(packet,j)
+                packet = []
+            j += 1
+        self.send_packet(packet,j)
+        print(j)
+        return j
+
+    def get_Decada(self):
+        today =datetime.date.today().strftime('%d')
+        month = str(int(datetime.date.today().strftime('%m')) - 1) if int(datetime.date.today().strftime('%m')) != 1 else '12'
+        year = str(int(datetime.date.today().strftime('%Y'))) if int(month) != 1 else str(int(datetime.date.today().strftime('%Y'))-1)
+        last_day=str(monthrange(int(year), int(month))[1])
+        date_start = f'01.{month}.{year}'
+        if today==5 or read_ini(self.conf,'DECADA',self.conf)=='5':
+            #month
+            date_go=year+month
+            type='MonthSale'
+            date_end = f'{last_day}.{month}.{year}'
+
+        if today==15 or read_ini(self.conf,'DECADA',self.conf)=='15':
+            #decade 1
+            date_go=year+month+'1'
+            type = 'DecadeSale'
+            date_end = f'10.{month}.{year}'
+
+        if today==25 or read_ini(self.conf,'DECADA',self.conf)=='25':
+            #decade 2
+            date_go=year+month+'2'
+            date_end = f'20.{month}.{year}'
+        self.fileID = self.getToken(date=date_go,type=type)
+        val = {'date_start': date_start, 'date_end': date_start, 'profile_id': self.profile_id}
+        self.data = self.DB.get_from_base(__name__, 'decada', val)
+        self.put_packet(self.getPacket_decada())
 
 
 
